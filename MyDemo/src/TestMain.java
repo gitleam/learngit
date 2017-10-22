fd83f478674f7e6f7e961f50724a8e91f493569d
jerry <jerry@example.com>
2017-10-22 15:22:00 +0800

    commit

---
+++ MyDemo/src/TestMain.java
@@ -0,0 +1,12 @@
+
+public class TestMain {
+
+	//注释
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("你好！");
+		System.out.println("你好啊");
+	}
+
+}
